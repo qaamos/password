6520ff5cd04c7c4cb72ff4ec33c447158813594b
Aamos Riihinen <n98arr@gmail.com>
2024-04-12 02:04:15 +0300

    Create barebones structure for project

---
+++ password.py
@@ -0,0 +1,24 @@
+from cryptography.fernet import Fernet
+import sqlite3
+import tkinter as tk
+
+def addPassword():
+    return
+
+def getPassword():
+    return
+
+def encryptPassword():
+    return
+
+def decryptPassword():
+    return
+
+def main():
+    mainWindow = tk.Tk()
+    mainWindow.title("Password Manager")
+    mainWindow.mainloop()
+    return 1
+
+if __name__ == "__main__":
+    main()
